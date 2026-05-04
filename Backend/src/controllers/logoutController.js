4c27d997ac19f0dcd8ef1311d9a51272fc5faa7f
Andriu07 <20240349@ricaldone.edu.sv>
2026-05-04 13:31:23 -0600

    Arreglo de error en Controllerlogout

--- Backend/src/controllers/logoutController.js
+++ Backend/src/controllers/logoutController.js
@@ -4,7 +4,7 @@ logoutContrroller.logout = async (req, res) =>{
     //Limpiar la cokkie que tiene la informacion de quien incio sesion
     res.clearCookie("authCookie");
 
-    return res-status(200).json({message:"Sesion cerrada"});
+    return res.status(200).json({message:"Sesion cerrada"});
 };
 
 export default logoutContrroller;
